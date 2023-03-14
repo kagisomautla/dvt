0e0d93767732e215a3d9780f8ec2c071e9c61dc4
Bambino95 <kagiso@doshex.com>
2023-03-14 23:54:22 +0200

    Create pop_up.dart

---
+++ lib/controls/pop_up.dart
@@ -0,0 +1,83 @@
+import 'package:dvt/controls/text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+Future<dynamic> popupControl({
+  required BuildContext context,
+  required String message,
+  required String title,
+  GestureTapCallback? onConfirm,
+}) async {
+  return await showDialog<void>(
+    context: context,
+    barrierDismissible: true,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Column(
+          children: [
+            Container(
+              height: 30,
+              margin: EdgeInsets.only(bottom: 10),
+              child: Icon(
+                FontAwesomeIcons.triangleExclamation,
+                color: Colors.orange,
+                size: 30,
+              ),
+            ),
+            TextControl(
+              text: title,
+              size: TextProps.md,
+              isBold: true,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        contentPadding: EdgeInsets.all(40),
+        actionsPadding: EdgeInsets.all(0),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextControl(
+              text: message,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: onConfirm,
+                  child: Container(
+                    color: Colors.black,
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                      child: TextControl(
+                        text: 'Yes',
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Container(
+                    color: Colors.grey,
+                    padding: EdgeInsets.all(20),
+                    child: Center(
+                      child: TextControl(
+                        text: 'No',
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+    },
+  );
+}
