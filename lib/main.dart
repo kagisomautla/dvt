67ac5e07ad1aa919a27df5530515021a7ab38dee
Bambino95 <kagiso@doshex.com>
2023-03-14 23:53:45 +0200

    update: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
+import 'package:dvt/providers/locations.dart';
+import 'package:dvt/providers/system.dart';
+import 'package:dvt/screens/favorites/favorites.dart';
+import 'package:dvt/screens/google_map/google_map.dart';
 import 'package:dvt/screens/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 
 Future main() async {
   await dotenv.load(fileName: "assets/.env");
-
   runApp(const WeatherApplication());
 }
 
 class WeatherApplication extends StatelessWidget {
   const WeatherApplication({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
@@ -19,16 +22,23 @@ class WeatherApplication extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<LocationsProvider>(create: (_) => LocationsProvider()),
+        ChangeNotifierProvider<SystemProvider>(create: (_) => SystemProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        routes: <String, WidgetBuilder>{
+          '/': (BuildContext context) => HomeScreen(),
+          '/favorites': (BuildContext context) => FavoritesScreen(),
+          '/google_map': (BuildContext context) => GoogleMapScreen(),
+        },
       ),
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomeScreen(),
-      },
     );
   }
 }
-
